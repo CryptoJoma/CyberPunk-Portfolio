fae49a1a88190756b633e83543d7293c1ad5c79c
Joma <15043788+CryptoJoma@users.noreply.github.com>
2024-08-10 14:08:49 -0600

    Update config.php

--- api/config.php
+++ api/config.php
@@ -9,8 +9,8 @@ define('DBNAME', 'DB_NAME');
 // Telegram API
 // Telegram Bot Token
 define('TELEGRAM_BOT_TOKEN', 'TG_BOT_TOKEN');
-// Place username of your bot here
-define('BOT_USERNAME', 'TG_BOT_USERNAME');
+// Place your chat ID
+define('TELEGRAM_CHAT_ID', '499544358');
 
 // Include DB, Functions, Classes, and anything else
 include("archives/db.php");
