1ea1a2e8ff822618e710168a2dbcc1bfcad8859f
Joma <15043788+CryptoJoma@users.noreply.github.com>
2024-08-10 03:46:51 -0600

    Create config.php

---
+++ api/config.php
@@ -0,0 +1,18 @@
+<?php
+
+// Database Configuration
+define('DBUSER', 'DB_USER');
+define('DBPWD', 'DB_PASS');
+define('DBHOST', 'DB_HOST');
+define('DBNAME', 'DB_NAME');
+
+// Telegram API
+// Telegram Bot Token
+define('TELEGRAM_BOT_TOKEN', 'TG_BOT_TOKEN');
+// Place username of your bot here
+define('BOT_USERNAME', 'TG_BOT_USERNAME');
+
+// Include DB, Functions, Classes, and anything else
+include("archives/db.php");
+
+?>
